971c7fa5f68afa54494ee7e0c58926efe61e97f2
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-09-11 01:10:20 -0400

    style: adding a home buttom and more basic styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { format } from "date-fns";
 import { FormEvent, useState } from "react";
 import { userAgentFromString } from "next/server";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const [command, setCommand] = useState<string[] | undefined>();
@@ -29,7 +30,12 @@ const Home: NextPage = () => {
       if (data.command === 'clear' && command[0] !== undefined) {
         setCommand([command[0]]);
       } else {
-        setCommand([...command, data.command]);
+        if (command.length > 20) {
+          setCommand([...command.slice(1,-1), data.command]);
+        } else {
+          setCommand([...command, data.command]);
+        }
+        
       }
       
     } else {
@@ -47,7 +53,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col gap-1 min-h-screen p-4 bg-zinc-900 cursor-text">
+      <main className="gap-1 min-h-screen p-4 bg-zinc-900 cursor-text divide-dashed divide-y-2">
+        <nav className="divide-y-4 mb-2">
+          <Link href="/">
+            <a className="text-zinc-400">1)home</a>
+          </Link>
+        </nav>
+        <div className="flex flex-col pt-2">
         <h1 className="mx-auto text-cyan-200">G.O.A.T. Terminal</h1>
         <div className="text-zinc-400">
           Last login: {format(new Date(), "PPpp")} :{" "}
@@ -63,24 +75,16 @@ const Home: NextPage = () => {
             if (idx === 0)
               return (
                 <div key={idx} className="text-zinc-400">
-                  <div className="text-zinc-400">
+                  <div className="text-zinc-400 mb-2">
                     <span className="text-green-500">$ </span>Hello {d}
                   </div>
-                  <div className="text-zinc-400">
+                  <div className="text-zinc-400 mb-2">
                     <span className="text-green-500">$ </span>Welcome to the Cardano G.O.A.T. Terminal
                   </div>
-                  <div className="text-zinc-400">
-                    <span className="text-green-500">$ </span>To purchase a Goat. Send 25 $ADA exactly to:
-                  </div>
-                  <div className="text-zinc-400 text-center items-center">
-                    addr1qx8j0xzjnvputf6dnfdyghmt4tguh622pmhwu69zdd8djfxkd2pzw443r70hsmrpdh4tnqelhdvssq5zm75fw8ht6psqgn4xep
-                  </div>
-                  <div className="text-zinc-400 text-center items-center">
-                    Do not send funds from exchanges or funds can be lost. Private wallets only.
-                  </div>
+                  
                 </div>
               );
-            if (d === "man")
+            if (d === 'man')
               return (
                 <div key={idx} className="text-zinc-400 text-center items-center">
                   <div className="text-green-500">$ </div>
@@ -91,9 +95,25 @@ const Home: NextPage = () => {
                     <div className="mb-2">Check back to the manual if you forget what commands you can use.</div>
                     <div className="mb-2">man - terminal manual</div>
                     <div className="mb-2">clear - clear terminal screen</div>
+                    <div className="mb-2">buy goat - info on how to purchase Cardano G.O.A.T. NFT</div>
+                    <div className="mb-2">learn cardano - start a lesson on how to use Cardano</div>
                   </div>
                 </div>
               );
+            if (d === 'buy goat')
+            return (
+              <div key={idx} className="text-zinc-400">
+                <div className="text-zinc-400 mb-2">
+                <span className="text-green-500">$ </span>To purchase a Goat. Send 25 $ADA exactly to:
+                </div>
+                <div className="text-zinc-400 text-center items-center mb-2">
+                addr1qx8j0xzjnvputf6dnfdyghmt4tguh622pmhwu69zdd8djfxkd2pzw443r70hsmrpdh4tnqelhdvssq5zm75fw8ht6psqgn4xep
+                </div>
+                <div className="text-zinc-400 text-center items-center mb-2">
+                Do not send funds from exchanges or funds can be lost. Private wallets only.
+                </div>
+              </div>
+            );
             return (
               <div key={idx} className="text-zinc-400">
                 <span className="text-green-500">$ </span>Command: {d} - to
@@ -101,30 +121,31 @@ const Home: NextPage = () => {
               </div>
             );
           })}
-        <div>
-          {}
-          <form onSubmit={handleSubmit}>
-            <span className="text-green-500">$ </span>
-            <span className="text-zinc-400">goat@browser</span>
-            <span className="text-cyan-500"> ~ </span>
-            <span className="text-green-500">-&gt; </span>
-            <label htmlFor="terminal" hidden>
-              Terminal Simulation
-            </label>
-            <input
-              type="text"
-              id="terminal"
-              name="terminal"
-              placeholder="_"
-              className="w-90 bg-zinc-900 text-zinc-400"
-              required
-              autoComplete="off"
-            />
-            <button type="submit" hidden>
-              Submit
-            </button>
-          </form>
-        </div>
+          </div>
+          <div className="flex flex-col">
+            <form onSubmit={handleSubmit}>
+              <span className="text-green-500">$ </span>
+              <span className="text-zinc-400">goat@browser</span>
+              <span className="text-cyan-500"> ~ </span>
+              <span className="text-green-500">-&gt; </span>
+              <label htmlFor="terminal" hidden>
+                Terminal Simulation
+              </label>
+              <input
+                type="text"
+                id="terminal"
+                name="terminal"
+                placeholder="_"
+                className="bg-zinc-900 text-zinc-400 min-w-[80%]"
+                required
+                autoComplete="off"
+                maxLength={50}
+              />
+              <button type="submit" hidden>
+                Submit
+              </button>
+            </form>
+          </div>
       </main>
     </>
   );
