62045f32bbeff9b99494828d6b15017ecb2fa543
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-09-11 02:09:10 -0400

    feat: lowering command window and adding the social media links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,14 +30,13 @@ const Home: NextPage = () => {
       if (data.command === 'clear' && command[0] !== undefined) {
         setCommand([command[0]]);
       } else {
-        if (command.length > 20) {
-          setCommand([...command.slice(1,-1), data.command]);
+        if (command.length > 2) {
+          setCommand([...command.slice(0,1),...command.slice(-2), data.command]);
         } else {
           setCommand([...command, data.command]);
         }
         
       }
-      
     } else {
       setCommand([data.command]);
     }
@@ -53,22 +52,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="gap-1 min-h-screen p-4 bg-zinc-900 cursor-text divide-dashed divide-y-2">
-        <nav className="divide-y-4 mb-2">
-          <Link href="/">
-            <a className="text-zinc-400">1)home</a>
-          </Link>
+      <main className="gap-1 min-h-screen p-4 bg-zinc-900 cursor-text divide-dashed hover:divide-y">
+        <nav className="mb-2 grid grid-cols-4">
+          <div>
+            <Link href="/">
+              <a className="text-zinc-400">1)home</a>
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              <a className="text-zinc-400">2)bash</a>
+            </Link>
+          </div>
         </nav>
         <div className="flex flex-col pt-2">
-        <h1 className="mx-auto text-cyan-200">G.O.A.T. Terminal</h1>
+        <h1 className="mx-auto text-indigo-300">G.O.A.T. Terminal</h1>
         <div className="text-zinc-400">
-          Last login: {format(new Date(), "PPpp")} :{" "}
+          Last Command: {format(new Date(), "PPpp")} :{" "}
           {userAgentFromString(undefined).os.name} :{" "}
           {userAgentFromString(undefined).browser.name}
         </div>
         <div className="text-zinc-400">
         <span className="text-green-500">$ </span>
-          Please enter your name. We hold no data. So we ask you every time. 
+          Please enter your name. 
         </div>
         {command &&
           command.map(function (d, idx) {
@@ -86,31 +92,36 @@ const Home: NextPage = () => {
               );
             if (d === 'man')
               return (
-                <div key={idx} className="text-zinc-400 text-center items-center">
-                  <div className="text-green-500">$ </div>
-                  <div className="mb-2">
-                    Goat Terminal Manual
-                  </div>
-                  <div>
-                    <div className="mb-2">Check back to the manual if you forget what commands you can use.</div>
-                    <div className="mb-2">man - terminal manual</div>
-                    <div className="mb-2">clear - clear terminal screen</div>
-                    <div className="mb-2">buy goat - info on how to purchase Cardano G.O.A.T. NFT</div>
-                    <div className="mb-2">learn cardano - start a lesson on how to use Cardano</div>
-                  </div>
-                </div>
+                <><div key={idx} className="text-zinc-400 mb-2">
+                  <span className="text-green-500">$ </span>Command: {d}
+                </div><div key={idx} className="text-zinc-400 text-center items-center">
+                    <div className="text-green-500">$ </div>
+                    <div className="mb-2">
+                      Goat Terminal Manual
+                    </div>
+                    <div>
+                      <div className="mb-2">Check back to the manual if you forget what commands you can use.</div>
+                      <div className="mb-2">man - terminal manual</div>
+                      <div className="mb-2">clear - clear terminal screen</div>
+                      <div className="mb-2">buy goat - info on how to purchase Cardano G.O.A.T. NFT</div>
+                      <div className="mb-2">social links - print out links to social media</div>
+                    </div>
+                  </div></>
               );
             if (d === 'buy goat')
             return (
               <div key={idx} className="text-zinc-400">
                 <div className="text-zinc-400 mb-2">
-                <span className="text-green-500">$ </span>To purchase a Goat. Send 25 $ADA exactly to:
+                <span className="text-green-500">$ </span>Command: {d}
+                </div>
+                <div className="text-zinc-400 mb-2">
+                <span className="text-green-500">$ </span><span className="text-cyan-400">To purchase a Goat. Send 25 $ADA exactly to:</span>
+                </div>
+                <div className="text-zinc-400 text-center items-center mb-2 break-words">
+                <span className="text-indigo-400 select-all">addr1qx8j0xzjnvputf6dnfdyghmt4tguh622pmhwu69zdd8djfxkd2pzw443r70hsmrpdh4tnqelhdvssq5zm75fw8ht6psqgn4xep</span>
                 </div>
                 <div className="text-zinc-400 text-center items-center mb-2">
-                addr1qx8j0xzjnvputf6dnfdyghmt4tguh622pmhwu69zdd8djfxkd2pzw443r70hsmrpdh4tnqelhdvssq5zm75fw8ht6psqgn4xep
-                </div>
-                <div className="text-zinc-400 text-center items-center mb-2">
-                Do not send funds from exchanges or funds can be lost. Private wallets only.
+                <span className="text-red-400">Do not send funds from exchanges or funds can be lost. Private wallets only.</span>
                 </div>
               </div>
             );
@@ -122,7 +133,7 @@ const Home: NextPage = () => {
             );
           })}
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col mt-2 pt-2">
             <form onSubmit={handleSubmit}>
               <span className="text-green-500">$ </span>
               <span className="text-zinc-400">goat@browser</span>
@@ -136,7 +147,7 @@ const Home: NextPage = () => {
                 id="terminal"
                 name="terminal"
                 placeholder="_"
-                className="bg-zinc-900 text-zinc-400 min-w-[80%]"
+                className="bg-zinc-900 text-zinc-400 min-w-[40%]"
                 required
                 autoComplete="off"
                 maxLength={50}
